fc8ad61dbaa2a7653bdd8162e0d1481772e7ce04
Senn <severin@senn-online.ch>
2021-12-08 15:04:13 +0100

    Update Actions.js

--- Frontend/src/store/auth/actions.js
+++ Frontend/src/store/auth/actions.js
@@ -1,13 +1,9 @@
 export const login = (context, obj) => {
-    context.commit("setJWT", obj.jwt);
-    console.log(obj.jwt)
-    axios.defaults.headers.common['authorization'] = "Bearer " + obj.jwt;
+  context.commit("setJWT", obj.jwt);
+  console.log(obj.jwt);
+  axios.defaults.headers.common["authorization"] = "Bearer " + obj.jwt;
 };
 export const logout = (context) => {
-    context.commit("setJWT", null);
-    axios.defaults.headers.common['authorization'] = null;
+  context.commit("setJWT", null);
+  axios.defaults.headers.common["authorization"] = null;
 };
-
-
-
-
